87ff876d95bd2a95c7b0b40abb3623d2a1a25079
KanikaSharma1999 <meetkanikasharma01011999@gmail.com>
2020-07-24 13:55:05 +0530

    first commit

---
+++ basic.dart
@@ -0,0 +1,13 @@
+//import 'test1.dart';
+
+main() {
+  var x = {
+    'id': 1,
+    "name": "kanika",
+    "lastname": "sharma",
+    "email": "ks@gm.com",
+  };
+  var email = x["email"];
+  print(email);
+  print(x.runtimeType);
+}
